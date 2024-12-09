003d1531bf68662343a52756ad72c77260c26899
shrfrn <shrfrn@gmail.com>
2024-12-09 12:47:59 +0200

    Add click, zoom & pan to map demo

--- js/geo-location.js
+++ js/geo-location.js
@@ -1,70 +1,89 @@
 'use strict'
 
+var gZoomLevel = 10
+var gMap
+
 function mapReady() {
-    console.log('Map is ready')
+	console.log('Map is ready')
 }
 
 function getPosition() {
+	if (!navigator.geolocation) {
+		alert('HTML5 Geolocation is not supported in your browser')
+		return
+	}
 
-    if (!navigator.geolocation) {
-        alert('HTML5 Geolocation is not supported in your browser')
-        return
-    }
+	// One shot position retrieval...
+	navigator.geolocation.getCurrentPosition(showLocation, handleLocationError)
 
-    // One shot position retrieval...
-    navigator.geolocation.getCurrentPosition(showLocation, handleLocationError)
-    
-    // ...or continous watch
-    // navigator.geolocation.watchPosition(showLocation, handleLocationError)
+	// ...or continous watch
+	// navigator.geolocation.watchPosition(showLocation, handleLocationError)
 }
 
 function showLocation(position) {
-    
-    console.log(position)
-    const { latitude: lat, longitude: lng, accuracy } = position.coords
+	console.log(position)
+	const { latitude: lat, longitude: lng, accuracy } = position.coords
 
-    document.getElementById("latitude").innerText = lat
-    document.getElementById("longitude").innerText = lng
-    document.getElementById("accuracy").innerText = accuracy
+	document.getElementById('latitude').innerText = lat
+	document.getElementById('longitude').innerText = lng
+	document.getElementById('accuracy').innerText = accuracy
 
-    var date = new Date(position.timestamp)
-    document.getElementById("timestamp").innerText = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
-    
-    initMap(lat, lng)
+	var date = new Date(position.timestamp)
+	document.getElementById('timestamp').innerText = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+
+	initMap(lat, lng)
 }
 
 function initMap(lat = 31, lng = 31) {
+	const elMap = document.querySelector('.map')
+	const mapOptions = {
+		center: { lat, lng },
+		zoom: gZoomLevel,
+	}
+	gMap = new google.maps.Map(elMap, mapOptions)
 
-    const elMap = document.querySelector('.map')
-    const mapOptions = {
-        center: { lat, lng },
-        zoom: 10
-    }
-    const map = new google.maps.Map(elMap, mapOptions)
+	const markerOptions = {
+		position: { lat, lng },
+		gMap,
+		title: 'Hello World!',
+	}
+	const marker = new google.maps.Marker(markerOptions)
+	setupMap(gMap)
+}
 
-    const markerOptions = {
-        position: { lat, lng },
-        map,
-        title: 'Hello World!'
-    }
-    const marker = new google.maps.Marker(markerOptions)
+function setupMap() {
+	gMap.addListener('click', ev => {
+		console.log(ev.latLng.lat(), ev.latLng.lng())
+	})
+}
+
+function zoom(diff) {
+	gZoomLevel += diff
+	gMap.setZoom(gZoomLevel)
+}
+
+function pan() {
+	const strLatLng = prompt('Enter lat, lng')
+	const latLng = strLatLng.split(',')
+
+	gMap.setCenter({ lat: +latLng[0], lng: +latLng[1] })
 }
 
 function handleLocationError(error) {
-    var locationError = document.getElementById("locationError")
+	var locationError = document.getElementById('locationError')
 
-    switch (error.code) {
-        case 0:
-            locationError.innerHTML = "There was an error while retrieving your location: " + error.message
-            break
-        case 1:
-            locationError.innerHTML = "The user didn't allow this page to retrieve a location."
-            break
-        case 2:
-            locationError.innerHTML = "The browser was unable to determine your location: " + error.message
-            break
-        case 3:
-            locationError.innerHTML = "The browser timed out before retrieving the location."
-            break
-    }
+	switch (error.code) {
+		case 0:
+			locationError.innerHTML = 'There was an error while retrieving your location: ' + error.message
+			break
+		case 1:
+			locationError.innerHTML = "The user didn't allow this page to retrieve a location."
+			break
+		case 2:
+			locationError.innerHTML = 'The browser was unable to determine your location: ' + error.message
+			break
+		case 3:
+			locationError.innerHTML = 'The browser timed out before retrieving the location.'
+			break
+	}
 }
