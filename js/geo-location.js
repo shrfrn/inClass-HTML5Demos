80ca08ce6b492ac906a19aaaa4a7858e8021091d
shrfrn <shrfrn@gmail.com>
2024-12-09 21:25:54 +0200

    Bug fix

--- js/geo-location.js
+++ js/geo-location.js
@@ -44,7 +44,7 @@ function initMap(lat = 31, lng = 31) {
 
 	const markerOptions = {
 		position: { lat, lng },
-		gMap,
+		map: gMap,
 		title: 'Hello World!',
 	}
 	const marker = new google.maps.Marker(markerOptions)
@@ -70,20 +70,20 @@ function pan() {
 }
 
 function handleLocationError(error) {
-	var locationError = document.getElementById('locationError')
+	var elLocationError = document.getElementById('locationError')
 
 	switch (error.code) {
 		case 0:
-			locationError.innerHTML = 'There was an error while retrieving your location: ' + error.message
+			elLocationError.innerHTML = 'There was an error while retrieving your location: ' + error.message
 			break
 		case 1:
-			locationError.innerHTML = "The user didn't allow this page to retrieve a location."
+			elLocationError.innerHTML = "The user didn't allow this page to retrieve a location."
 			break
 		case 2:
-			locationError.innerHTML = 'The browser was unable to determine your location: ' + error.message
+			elLocationError.innerHTML = 'The browser was unable to determine your location: ' + error.message
 			break
 		case 3:
-			locationError.innerHTML = 'The browser timed out before retrieving the location.'
+			elLocationError.innerHTML = 'The browser timed out before retrieving the location.'
 			break
 	}
 }
